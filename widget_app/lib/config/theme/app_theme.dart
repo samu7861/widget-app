ff89425f591e2c6add7024d4633dd39cbb360416
samu7861 <sfrancog7861@gmail.com>
2024-05-23 16:00:49 -0500

    HomeScreen and ThemeConfig

---
+++ widget_app/lib/config/theme/app_theme.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+const List<Color> colorList = [
+  Colors.red,
+  Colors.green,
+  Colors.blue,
+  Colors.yellow,
+  Colors.purple,
+  Colors.orange,
+  Colors.pink,
+];
+
+class AppTheme {
+
+  final int selectedColor;
+  
+
+  AppTheme({required this.selectedColor}):assert(selectedColor >= 0 && selectedColor < colorList.length);
+
+  ThemeData getThemeData() {
+    return ThemeData(
+      colorScheme: ColorScheme.fromSeed(seedColor: colorList[selectedColor]),
+      useMaterial3: true,
+      appBarTheme: const AppBarTheme(
+        centerTitle: true,
+      ),
+    );
+  }
+
+}
---
+++ widget_app/lib/presentation/screens/home/home_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter + material3'),
+      ),
+    );
+  }
+}
