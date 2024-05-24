15e00541610bb5f3f4dd96dc5bf584824337e178
samu7861 <sfrancog7861@gmail.com>
2024-05-23 22:29:37 -0500

    ListTile

---
+++ widget_app/lib/config/menu/menu.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class MenuItems {
+  final String title;
+  final String subTitle;
+  final String link;
+  final IconData icon;
+
+  const MenuItems({required this.title, required this.subTitle, required this.link, required this.icon});
+
+}
+const listMenu = <MenuItems>[
+  MenuItems(
+    title: "Botones", 
+    subTitle: "varios botones", 
+    link: "/buttons", 
+    icon: Icons.smart_button_outlined),
+
+    MenuItems(
+    title: "Tarjeta", 
+    subTitle: "Un contenedor estilizado", 
+    link: "/card", 
+    icon: Icons.credit_card)
+];
--- widget_app/lib/presentation/screens/home/home_screen.dart
+++ widget_app/lib/presentation/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:widget_app/config/menu/menu.dart';
+
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -10,6 +11,28 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Flutter + material3'),
       ),
+      body:const _HomeView(listMenu),
     );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+
+  final List<MenuItems> listMenu;
+
+  const _HomeView(this.listMenu);
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: listMenu.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(listMenu[index].title),
+        );
+      },
+      );
   }
 }
